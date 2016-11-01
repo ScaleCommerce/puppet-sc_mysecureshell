cb7c929ba97a2307cb3fd338d3a53ff1b1a9ee3c
Andreas Ziethen <az@scale.sc>
2016-11-01 13:44:47 +0100

    change define name

--- manifests/config_tag.pp
+++ manifests/config_tag.pp
@@ -1,16 +1,9 @@
-define config_tag(
+define sc_mysecureshell::config_tag(
   $type,
   $attribute,
   $settings = {},
-
 ) {
-
-
   file {"/etc/ssh/sftp.d/$type-$attribute.conf":
-
     content => template("${module_name}/config_tag.conf.erb"),
-
   }
-
-
 }
