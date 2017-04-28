a2dca28dde67f73d01ec44cbd085ab7aa4fda6c9
a.ziethen <az@scale.sc>
2017-04-28 21:11:07 +0200

    add more config params for template default.conf.erb, increase version

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,8 +24,74 @@
 #
 # Copyright ScaleCommerce GmbH 2016
 #
-class sc_mysecureshell (
+# === Parameters
+#
+# [*global_download*]
+#   total speed download for all clients
+#
+# [*global_upload*]
+#   total speed download for all clients (0 for unlimited)
+#
+# [*download*]
+#   limit speed download for each connection
+#
+# [*upload*]
+#   unlimit speed upload for each connection
+#
+# [*stay_at_home*]
+#   limit client to his home
+#
+# [*virtual_chroot*]
+#   fake a chroot to the home account
+#
+# [*limit_connection*]
+#   max connection for the server sftp
+#
+# [*limit_connection_by_user*]
+#   max connection for the account
+#
+# [*limit_connection_by_ip*]
+#   max connection by ip for the account
+#
+# [*home*]
+#   overrite home of the user but if you want you can use
+#
+# [*idle_time_out*]
+#   (in second) deconnect client is idle too long time
+#
+# [*resolve_ip*]
+#   resolve ip to dns
+#
+# [*ignore_hidden*]
+#   treat all hidden files as if they don't exist
+#
+# [*hide_no_access*]
+#   Hide file/directory which user has no access
+#
+# [*default_rights*]
+#   Set default rights for new file and new directory
+#
+# [*show_links_as_links*]
+#   show links as their destinations
+#
 
+class sc_mysecureshell (
+  $global_download          = 0,	            #total speed download for all clients
+  $global_upload            = 0,	            #total speed download for all clients (0 for unlimited)
+  $download                 = 0,	            #limit speed download for each connection
+  $upload                   = 0,	            #unlimit speed upload for each connection
+  $stay_at_home             = true,	          #limit client to his home
+  $virtual_chroot           = true,	          #fake a chroot to the home account
+  $limit_connection         = 10,	            #max connection for the server sftp
+  $limit_connection_by_user =	10,	            #max connection for the account
+  $limit_connection_by_ip   = 10,	            #max connection by ip for the account
+  $home                     = '/home/$USER',	#overrite home of the user but if you want you can use
+  $idle_time_out            = '5m',	          #(in second) deconnect client is idle too long time
+  $resolve_ip               = false,	        #resolve ip to dns
+  $ignore_hidden            = true,	          #treat all hidden files as if they don't exist
+  $hide_no_access           = true,	          #Hide file/directory which user has no access
+  $default_rights           = '0640 0750',	  #Set default rights for new file and new directory
+  $show_links_as_links      = false,	        #show links as their destinations
 ) {
   case $::operatingsystem {
     'Ubuntu': {
