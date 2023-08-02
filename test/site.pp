becae4f441c480a76975ece35f7a553959d3bdd3
Markus Schröder <ms@scale.sc>
2023-08-02 15:43:41 +0200

    fix lookup syntax

--- test/site.pp
+++ test/site.pp
@@ -1,23 +1,23 @@
 lookup('classes')
 
 # resource wrapper
-$files=lookup('files', {})
+$files=lookup('files', undef, undef, {})
 create_resources(file, $files)
 
-$crons=lookup('crons', {})
+$crons=lookup('crons', undef, undef, {})
 create_resources(cron, $crons)
 
-$execs=lookup('execs', {})
+$execs=lookup('execs', undef, undef, {})
 create_resources(exec, $execs)
 
-$hosts=lookup('hosts', {})
+$hosts=lookup('hosts', undef, undef, {})
 create_resources(host, $hosts)
 
-$mounts=lookup('mounts', {})
+$mounts=lookup('mounts', undef, undef, {})
 create_resources(mount, $mounts)
 
-$packages=lookup('packages', {})
+$packages=lookup('packages', undef, undef, {})
 create_resources(package, $packages)
 
-$services=lookup('services', {})
+$services=lookup('services', undef, undef, {})
 create_resources(service, $services)
